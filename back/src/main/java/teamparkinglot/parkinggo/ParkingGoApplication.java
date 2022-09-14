343f18561acb080aee226c0564b18a2917fa9869
Crazykwak <kwj1830@naver.com>
2022-09-14 20:31:35 +0900

    백 폴더 추가

---
+++ back/src/main/java/teamparkinglot/parkinggo/ParkingGoApplication.java
@@ -0,0 +1,13 @@
+package teamparkinglot.parkinggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingGoApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/teamparkinglot/parkinggo/ParkingGoApplicationTests.java
@@ -0,0 +1,13 @@
+package teamparkinglot.parkinggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
