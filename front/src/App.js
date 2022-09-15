d3bee845e147dfd508c4d970cfe27fdb28ef6071
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-15 16:01:21 +0900

    fix : global layout

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./layout.css"; // 레이아웃 CSS 입니다. Don't touch !
+import "./App.css"; // 비어있으니 레이아웃 외 CSS 추가변경 원하시면 이곳에서 수정해주세요 !
+import HealthCheck from "./test/HealthCheck";
+import Header from "./component/Header/Header";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="container">
+      <div className="item side">프로젝트 소개 구역</div>
+      <div className="item main_container">
+        <div className="item header">
+          <Header />
+        </div>
+        <div className="main">
+          {/* ↓ 아래 main div 안에 페이지 추가해주시면 됩니다. */}
+          <HealthCheck />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
