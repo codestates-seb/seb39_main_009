543766ba9e55f983db9e91908f11454bca0c9eec
Crazykwak <kwj1830@naver.com>
2022-09-20 16:38:09 +0900

    smtp 메일 보내기 기능은 완성 (정리 필요)

--- back/src/main/java/teamparkinglot/parkinggo/member/controller/MemberController.java
+++ back/src/main/java/teamparkinglot/parkinggo/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package teamparkinglot.parkinggo.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,7 +9,6 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import teamparkinglot.parkinggo.member.dto.MemberJoinDto;
-import teamparkinglot.parkinggo.member.dto.MemberLoginDto;
 import teamparkinglot.parkinggo.member.dto.ResetPwdDto;
 import teamparkinglot.parkinggo.member.entity.Member;
 import teamparkinglot.parkinggo.member.mapper.MemberMapper;
@@ -23,6 +23,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -31,7 +32,7 @@ public class MemberController {
     private final JavaMailSender javaMailSender;
 
     private final SecretCode secretCode;
-    private String from = "파킹Go";
+    private String from = "kwj1830@gmail.com";
 
     @PostMapping("/join")
     public ResponseEntity joinUser(@Valid @RequestBody MemberJoinDto memberJoinDto) {
@@ -44,19 +45,20 @@ public class MemberController {
     }
 
     @PostMapping("/resetpwd")
-    public ResponseEntity resetPwdSendEmail(@RequestBody String email) throws MessagingException {
+    public ResponseEntity resetPwdSendEmail(@RequestBody ResetPwdDto email) throws MessagingException {
 
         UUID uuid = UUID.randomUUID();
-
+        log.info("비밀번호 재설정을 위한 메일 발송 로직 진입");
+        log.info("보낼 메일 주소 : {}", email);
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
         mimeMessageHelper.setFrom(from);
-        mimeMessageHelper.setTo(email);
+        mimeMessageHelper.setTo(email.getEmail());
         mimeMessageHelper.setSubject("[파킹Go] 비밀번호 재설정 주소 안내");
 
         StringBuilder sb = new StringBuilder();
         sb.append("비밀번호 재설정을 위한 주소입니다. \n");
-        sb.append(secretCode.getClientUrl() + "/resetpwd" + uuid + "\n");
+        sb.append(secretCode.getClientUrl() + "/resetpwd/" + uuid + "\n");
         sb.append("상기 주소로 접속하시어 비밀번호 재설정을 해주시기 바랍니다.");
 
         mimeMessageHelper.setText(sb.toString(), true);
