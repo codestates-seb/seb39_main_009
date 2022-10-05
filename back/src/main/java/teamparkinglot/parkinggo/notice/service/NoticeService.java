85ca23934efd7d594f7f9cbd47f48681bcc38844
Crazykwak <kwj1830@naver.com>
2022-10-05 23:43:54 +0900

    fix : Notice 페이징 적용 (응답은 그대로임)

--- back/src/main/java/teamparkinglot/parkinggo/notice/service/NoticeService.java
+++ back/src/main/java/teamparkinglot/parkinggo/notice/service/NoticeService.java
@@ -1,6 +1,10 @@
 package teamparkinglot.parkinggo.notice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import teamparkinglot.parkinggo.notice.dto.NoticeResDto;
@@ -16,9 +20,11 @@ public class NoticeService {
     private final NoticeRepository noticeRepository;
 
     @Transactional
-    public List<NoticeResDto> viewNotices() {
+    public Page<NoticeResDto> viewNotices() {
 
-        List<NoticeResDto> noticeList =  noticeRepository.findNoticeAll();
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").descending());
+
+        Page<NoticeResDto> noticeList =  noticeRepository.findNoticeAll(pageable);
 
         return noticeList;
     }
