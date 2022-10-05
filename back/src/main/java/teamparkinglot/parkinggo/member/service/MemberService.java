27d2ead64b73fb02e118ebcefb13216fb5221758
Crazykwak <kwj1830@naver.com>
2022-10-05 22:59:10 +0900

    error : MemberService에 ReservationQueryDsl 주입 제거

--- back/src/main/java/teamparkinglot/parkinggo/member/service/MemberService.java
+++ back/src/main/java/teamparkinglot/parkinggo/member/service/MemberService.java
@@ -12,6 +12,7 @@ import teamparkinglot.parkinggo.member.dto.ReservationListDto;
 import teamparkinglot.parkinggo.member.dto.SidebarDto;
 import teamparkinglot.parkinggo.member.entity.Member;
 import teamparkinglot.parkinggo.member.repository.MemberRepository;
+import teamparkinglot.parkinggo.reservation.repository.ReservationRepository;
 import teamparkinglot.parkinggo.reservation.repository.ReservationRepositoryQueryDsl;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final ReservationRepositoryQueryDsl reservationRepositoryQueryDsl;
+    private final ReservationRepository reservationRepository;
 
     @Transactional
     public Member memberCreate(Member member) {
@@ -80,7 +81,7 @@ public class MemberService {
 
     public List<ReservationListDto> viewReservations(String email) {
 
-        return reservationRepositoryQueryDsl.findReservationList(email);
+        return reservationRepository.findReservationList(email);
     }
 
     @Transactional
